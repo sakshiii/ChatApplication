6d1e5ed5819392103ef243f11233bdcc6591fc9e
sakshiii <sakshikumsr53@gmail.com>
2020-09-17 02:21:02 +0530

    basic working

--- server/index.js
+++ server/index.js
@@ -1,1 +1,26 @@
-const express=
+const express=require("express");
+const socketio=require("socket.io");
+const http=require("http");
+
+const app=express();
+const PORT=process.env.PORT || 5000;
+
+const router=require('./router');
+
+
+const server= http.createServer(app)
+const io= socketio(server);
+
+io.on('connection', (socket)=>{
+
+    console.log("We have a connection!!");
+
+    socket.on('disconnect',()=>{
+        console.log('User has left!!')
+    })
+})
+
+app.use(router);
+
+server.listen(PORT,()=>console.log(`Server has started on port ${PORT}`))
+
